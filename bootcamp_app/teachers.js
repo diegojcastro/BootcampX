540c93899727aafbcd1fa8df9cf7cb0cd2a694fd
Diego Castro <diegojcr4@gmail.com>
2021-09-15 17:27:15 -0400

    add connection between JS and DB

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT distinct teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher} `);
+  })
+}).catch(err => console.error('query error', err.stack));
